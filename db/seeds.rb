1473d06a5c8429dce23e3411cd326f2924ae8f93
Kasimir von Halem <kasimir.halem@me.com>
2020-06-10 14:47:16 +0200

    routs are done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "creating restaurants..."
+Restaurant.destroy_all
+
+laFonda = { name: "LaFonda", address: "Köln Innenstadt", category: "italian"}
+fetteKuh = { name: "Fette Kuh", address: "Köln Innenstadt", category: "italian"}
+threeHs = { name: "threeHs", address: "Köln Innenstadt", category: "italian"}
+höhns = { name: "Höhns", address: "Köln Innenstadt", category: "italian"}
+johsephs = { name: "Joshsephs", address: "Köln Innenstadt", category: "italian"}
+Restaurant.create(laFonda)
+Restaurant.create(fetteKuh)
+Restaurant.create(threeHs)
+Restaurant.create(höhns)
+Restaurant.create(johsephs)
+puts "Here are the Restaurants!"
